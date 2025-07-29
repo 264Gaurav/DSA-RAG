e5c6bd1b1cc0c3d6ad31e21522a082a1c4997f56
GAURAV SINGH <gauravsingh264209@gmail.com>
2025-07-30 02:13:55 +0530

    adjusted chunk size in index.js and server side beutifying user question

--- index.js
+++ index.js
@@ -5,21 +5,16 @@ import { GoogleGenerativeAIEmbeddings } from '@langchain/google-genai';
 import { Pinecone } from '@pinecone-database/pinecone';
 import { PineconeStore } from '@langchain/pinecone';
 
-
-
 dotenv.config();
 
-const CHUNK_SIZE = 1000; // Size of each chunk
-const CHUNK_OVERLAP = 200; // Overlap between chunks
-
+const CHUNK_SIZE = 500; // Size of each chunk
+const CHUNK_OVERLAP = 50; // Overlap between chunks
 
 // Load the PDF file
 const PDF_PATH = './assets/dsa.pdf';
 const pdfLoader = new PDFLoader(PDF_PATH);
 const rawDocs = await pdfLoader.load();
 //console.log(rawDocs.length)
-
-
 
 // Create the chunk of the PDF
 const textSplitter = new RecursiveCharacterTextSplitter({
@@ -31,15 +26,11 @@ const chunkedDocs = await textSplitter.splitDocuments(rawDocs);
 //console.log(JSON.stringify(chunkedDocs.slice(0, 2), null, 2));
 //console.log(chunkedDocs.length);
 
-
-
-
 //Initializing the Embedding model
 const embeddings = new GoogleGenerativeAIEmbeddings({
   apiKey: process.env.GEMINI_API_KEY,
   model: 'text-embedding-004',
 });
-
 
 //Initialize Pinecone Client
 const pinecone = new Pinecone();
